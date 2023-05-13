dd141a5ccda0fe7a9669d2810c12d30603f5004e
Amogh Kulkarni <amogh.s.kulkarni@vanderbilt.edu>
2023-05-13 15:35:09 +0530

    Basic container component to standardize the page layout

--- domainTest/src/pages/Home.tsx
+++ domainTest/src/pages/Home.tsx
@@ -1,23 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonPage } from '@ionic/react';
 import './Home.css';
+import Container from '../components/basic/Container';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      <Container header='Test'>
+        <p>Hello</p>
+      </Container>
     </IonPage>
   );
 };
