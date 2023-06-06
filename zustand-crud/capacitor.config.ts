1d1ec180bc505aec3b920d04c7413bbf15eb3bd5
amoghkulkarnifr <amogh.kulkarni@fradium.com>
2023-06-06 19:47:04 +0530

    Renames

--- zustand-crud/capacitor.config.ts
+++ zustand-crud/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'domainTest',
+  appName: 'zustand-crud',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
