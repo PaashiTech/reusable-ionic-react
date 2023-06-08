15cab5f24ce7fe9dc5666879a6b7d37d16707749
amoghkulkarnifr <amogh.kulkarni@fradium.com>
2023-06-08 14:00:05 +0530

    Cleanup

--- zustand-crud/src/stores/goalStore.ts
+++ zustand-crud/src/stores/goalStore.ts
@@ -1,79 +1,82 @@
 import { create } from "zustand";
-import { Goal } from "../types/goal";
+import { devtools } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 import { 
+  // API types
   AddGoalInput, AddGoalOutput, AddGoalParams, 
   GetGoalInput, GetGoalOutput, GetGoalParams, 
   GetGoalsInput, GetGoalsOutput, GetGoalsParams,
   EditGoalInput, EditGoalOutput, EditGoalParams, 
   DeleteGoalInput, DeleteGoalOutput, DeleteGoalParams, 
+  // Store types
+  GoalStoreState, GoalStoreActions,
 } from "./types/goal";
-
-interface GoalStoreState {
-  /** Main container for Goals */
-  goals: Goal[],
-
-  /** Loading statuses for APIs */
-  addGoalLoading: boolean,
-  getGoalLoading: boolean,
-  getGoalsLoading: boolean,
-  editGoalLoading: boolean,
-  deleteGoalLoading: boolean,
-
-  /** Output of getGoal */
-  getGoalOutput: Goal | null,
-}
-
-interface GoalStoreActions {
-  /** CREATE */
-  addGoal: (params: AddGoalParams, data: AddGoalInput) => AddGoalOutput,
-
-  /** READ */
-  getGoal: (params: GetGoalParams, data: GetGoalInput) => GetGoalOutput,
-  getGoals: (params: GetGoalsParams, data: GetGoalsInput) => GetGoalsOutput,
-
-  /** UPDATE */
-  editGoal: (params: EditGoalParams, data: EditGoalInput) => EditGoalOutput,
-
-  /** DELETE */
-  deleteGoal: (params: DeleteGoalParams, data: DeleteGoalInput) => DeleteGoalOutput,
-}
 
 const useGoalStore = create<
   GoalStoreState 
   & GoalStoreActions
-  >() ((set, get) => ({
-  goals: [],
-  addGoalLoading: false,
-  getGoalLoading: false,
-  getGoalsLoading: false,
-  editGoalLoading: false,
-  deleteGoalLoading: false,
-  getGoalOutput: null,
+  >() (
+  immer(                          // To make state copying easier
+  devtools(                       // To make debugging tool avaialbe in the browser
+    (set, get) => ({
+    goals: [],
+    addGoalLoading: false,
+    getGoalLoading: false,
+    getGoalsLoading: false,
+    editGoalLoading: false,
+    deleteGoalLoading: false,
+    getGoalOutput: null,
 
-  // TODO: Add implementation of the CRUD API
-  addGoal(params: AddGoalParams, data: AddGoalInput) {
-    set((state) => ({ goals: state.goals.concat(data.goal)}));
-    return { data: null, status: 200 }
-  },
+    // TODO: Add implementation of the CRUD API
+    addGoal(params: AddGoalParams, data: AddGoalInput) {
+      let output: AddGoalOutput = { data: null, status: 200 }; 
+      
+      set((state) => { 
+        goals: state.goals.concat(data.goal) 
+      });
+      
+      return output;
+    },
 
-  getGoal(params, data) {
-    set((state) => state);
-    return { data: null, status: 200 }
-  },
+    getGoal(params: GetGoalParams, data: GetGoalInput) {
+      let output: GetGoalOutput = { data: null, status: 200 };
 
-  getGoals(params, data) {
-    set((state) => state);
-    return { data: null, status: 200 }
-  },
+      set((state) => {
+        state
+      });
+      
+      return output;
+    },
 
-  editGoal(params, data) {
-    set((state) => state);
-    return { data: null, status: 200 }
-  },
+    getGoals(params: GetGoalsParams, data: GetGoalsInput) {
+      let output: GetGoalsOutput = { data: null, status: 200 };
+      
+      set((state) => {
+        state
+      });
 
-  deleteGoal(params, data) {
-    set((state) => state);
-    return { data: null, status: 200 }
-  }
-  })
+      return output;
+    },
+
+    editGoal(params: EditGoalParams, data: EditGoalInput) {
+      let output: EditGoalOutput = { data: null, status: 200 };
+      
+      set((state) => {
+        state
+      });
+      
+      return output;
+    },
+
+    deleteGoal(params: DeleteGoalParams, data: DeleteGoalInput) {
+      let output: DeleteGoalOutput = { data: null, status: 200 };
+
+      set((state) => {
+        state
+      });
+
+      return output;
+    }
+    })
+  ))
 )
--- zustand-crud/src/stores/types/goal.ts
+++ zustand-crud/src/stores/types/goal.ts
@@ -1,64 +1,86 @@
 import { Goal } from "../../types/goal";
 
-// API params
+/** 
+ * Types for APIs 
+ */
+/** Params */
 export type AddGoalParams = null;
-
 export type GetGoalParams = { 
   id: string 
 };
-
 export type GetGoalsParams = null;
-
 export type EditGoalParams = {
   id: string
 };
-
 export type DeleteGoalParams = null;
 
-
-// API inputs
+/** Inputs */
 export type AddGoalInput = {
   goal: Goal
 };
-
 export type GetGoalInput = null;
-
 export type GetGoalsInput = null;
-
 export type EditGoalInput = {
   name: string,
   targetDate: string,     /** ISO string */
   lastUdpatedOn: string
 };
-
 export type DeleteGoalInput = {
   id: number
 };
 
-
-
-// API outputs 
+/** Outputs */
 export type AddGoalOutput = {
   status: number,
   data: null
 };
-
 export type GetGoalOutput = {
   status: number,
   data: Goal | null
 };
-
 export type EditGoalOutput = {
   status: number,
   data: null
 };
-
 export type DeleteGoalOutput = {
   status: number,
   data: null
 };
-
 export type GetGoalsOutput = {
   status: number,
   data: null
 };
+
+
+/**
+ * Types for the store
+ */
+export type GoalStoreState = {
+  /** Main container for Goals */
+  goals: Goal[],
+
+  /** Loading statuses for APIs */
+  addGoalLoading: boolean,
+  getGoalLoading: boolean,
+  getGoalsLoading: boolean,
+  editGoalLoading: boolean,
+  deleteGoalLoading: boolean,
+
+  /** Output of getGoal */
+  getGoalOutput: Goal | null,
+}
+
+export type GoalStoreActions = {
+  /** CREATE */
+  addGoal: (params: AddGoalParams, data: AddGoalInput) => AddGoalOutput,
+
+  /** READ */
+  getGoal: (params: GetGoalParams, data: GetGoalInput) => GetGoalOutput,
+  getGoals: (params: GetGoalsParams, data: GetGoalsInput) => GetGoalsOutput,
+
+  /** UPDATE */
+  editGoal: (params: EditGoalParams, data: EditGoalInput) => EditGoalOutput,
+
+  /** DELETE */
+  deleteGoal: (params: DeleteGoalParams, data: DeleteGoalInput) => DeleteGoalOutput,
+}
