bcf41cfabdfaa3658eb0e68b4f0c4039e6e75cd9
amoghkulkarnifr <amogh.kulkarni@fradium.com>
2023-06-22 00:30:46 +0530

    Storybook init example stories

--- storybook/.eslintrc.js
+++ storybook/.eslintrc.js
@@ -3,15 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:react/recommended',
-    'eslint:recommended'
-  ],
+  'extends': ['plugin:react/recommended', 'eslint:recommended', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-}
+};
